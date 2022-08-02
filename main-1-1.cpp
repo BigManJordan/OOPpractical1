56ee13714283ba1c4b290d488a67a65d539fb875
BigManJordan <100400782+BigManJordan@users.noreply.github.com>
2022-08-02 15:27:41 +0930

    Add files via upload

---
+++ main-1-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function-1-1.hpp"
+
+extern int sum_array(int*, int);
+
+int main(void){
+  
+  int array[5] = {1,2,3,4,5};
+  
+  int sum = sum_array(array,5);
+  printf("sum = %d\n", sum);
+}
